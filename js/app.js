a91f328cd6f2a57d07236fd270ed509db5e00301
Arafat Islam <arafat.innotimes.net@gmail.com>
2024-02-28 18:33:11 +0600

    added cart and all

---
+++ js/app.js
@@ -0,0 +1,88 @@
+const loadPhone = async (searchText , isShow) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
+    const data = await res.json()
+    const phones = data.data;
+    // console.log(phones)
+    displayPhones(phones ,isShow);
+};
+const displayPhones = (phones , isShow) => {
+   const phoneContainer = document.getElementById('phone-container');
+    phoneContainer.innerHTML = '';
+    const seeAllPhone = document.getElementById('see-all-phone');
+    if (phones.length > 9 && !isShow){
+        seeAllPhone.classList.remove('hidden');
+    }
+    else{
+        seeAllPhone.classList.add('hidden');
+    }
+
+    if (!isShow){
+        phones = phones.slice(0,9);
+    }
+    // console.log("is show", isShow);
+    phones.forEach(phone => {
+        console.log(phone)
+        const phoneCard = document.createElement('div');
+        phoneCard.classList = ` card bg-gray-100 shadow `;
+        phoneCard.innerHTML = `
+        <figure><img src="${phone.image}" alt="Shoes" /></figure>
+        <div class="card-body">
+          <h2 class="card-title">${phone.phone_name}</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div class="card-actions justify-center">
+            <button onclick="showAllDetails('${phone.slug}')" class="btn btn-primary ">Show Details</button>
+          </div>
+        </div>
+        `;
+        phoneContainer.appendChild(phoneCard);
+
+    });
+    toggleLoadingSpanners(false);
+
+}
+const handleSearch = (isShow) => {
+    toggleLoadingSpanners(true);
+    const searchText = document.getElementById('search-filed').value;
+    loadPhone(searchText , isShow);
+};
+
+toggleLoadingSpanners = (isLoading) => {
+    const loadingSpinner = document.getElementById('loading-spinner');
+    if(isLoading) {
+        loadingSpinner.classList.remove('hidden');
+    }
+    else{
+        loadingSpinner.classList.add('hidden');
+    }
+};
+
+const showAllDetails =async (id) => {
+    // console.log('showAllDetails', id);
+    const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`)
+    const data = await res.json()
+    
+    showPhoneDetail(data.data);
+}
+const showPhoneDetail =(phone) => {
+    console.log(phone);
+    const showDetailsPhoneName = document.getElementById('showDetailsPhoneName');
+    showDetailsPhoneName.innerText = phone.name;
+    const showDetailsImg = document.getElementById('showDetailsImg');
+    showDetailsImg.src = phone.image;
+    const DetailsPhoneShow = document.getElementById('DetailsPhoneShow')
+    DetailsPhoneShow.innerText = phone.mainFeatures.storage;
+    const showDetailDisplaySize = document.getElementById('showDetailDisplaySize')
+    showDetailDisplaySize.innerText = phone.mainFeatures.displaySize;
+    const showDetailChipset = document.getElementById('showDetailChipset')
+    showDetailChipset.innerText = phone.mainFeatures.chipSet;
+
+
+    showDetailsModal.showModal();
+};
+
+
+const seeAllPhone = document.getElementById('see-all-phone');
+seeAllPhone.addEventListener('click', () => {
+    handleSearch(true);
+});
+
